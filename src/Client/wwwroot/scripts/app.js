c76a6c9bf54c2818059615b4203684264621ffac
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-10-22 16:42:20 +0200

    datacard fix

--- src/Client/wwwroot/scripts/app.js
+++ src/Client/wwwroot/scripts/app.js
@@ -1,4 +1,7 @@
 ﻿//toggle navigation menu for narrow screens
+const marquees = document.getElementsByTagName("marquee").stop();
+
+
 const openMenu = () => {
     const menu = document.querySelector(".navigation-options");
     console.log(menu);
