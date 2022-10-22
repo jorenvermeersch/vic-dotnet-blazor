fb92255bc5dc93844fe4ee37448186ac8a61eeee
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-10-22 16:20:37 +0200

    navigation narrow screen fix + datacard css in blazor

---
+++ src/Client/wwwroot/scripts/app.js
@@ -0,0 +1,6 @@
+﻿//toggle navigation menu for narrow screens
+const openMenu = () => {
+    const menu = document.querySelector(".navigation-options");
+    console.log(menu);
+    menu.style.display = menu.style.display == "flex" ? "none" : "flex";
+};
