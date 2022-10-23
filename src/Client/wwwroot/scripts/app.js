b806ba3ad121da11a6fe530f1e6dfbd4e61fb8fb
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-10-23 14:38:17 +0200

    navigation narrow screen fix

--- src/Client/wwwroot/scripts/app.js
+++
@@ -1,9 +0,0 @@
-﻿//toggle navigation menu for narrow screens
-const marquees = document.getElementsByTagName("marquee").stop();
-
-
-const openMenu = () => {
-    const menu = document.querySelector(".navigation-options");
-    console.log(menu);
-    menu.style.display = menu.style.display == "flex" ? "none" : "flex";
-};
---
+++ src/Client/wwwroot/scripts/index.js
@@ -0,0 +1,4 @@
+﻿const openMenu = () => {
+    const menu = document.querySelector(".navigation-options");
+    menu.style.display = menu.style.display == "flex" ? "none" : "flex";
+};
