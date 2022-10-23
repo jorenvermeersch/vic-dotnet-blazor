53bd6c2e53fc3b1f4da8695f699962a18da4be92
Angela Degryse <angela.degryse@student.hogent.be>
2022-10-23 17:25:58 +0200

    departement veranderd naar enum

---
+++ src/Domain/Departement.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain;
+
+public enum Departement
+{
+    Bedrijf_en_Organisatie, 
+    Biowetenschappen_en_Industriële_Technologie, 
+    Gezondheidszorg, 
+    IT_en_Digitale_Innovatie,
+    Lerarenopleiding,
+    Omgeving,
+    Sociaal_Agogisch_Werk,
+    graduaatsopleidingen
+
+}
--- src/Domain/InternalCustomer.cs
+++ src/Domain/InternalCustomer.cs
@@ -3,12 +3,12 @@
 public class InternalCustomer : ICustomer
 {
     public string Education { get; set; }
-    public string Departement { get; set; }
+    public Departement Departement { get; set; }
     public int Id { get; set; }
     public ContactPerson ContactPerson { get; set; }
     public ContactPerson BackupContactPerson { get; set; }
 
-    public InternalCustomer(string education, string department, ContactPerson contactPerson, ContactPerson backupContact)
+    public InternalCustomer(string education, Departement department, ContactPerson contactPerson, ContactPerson backupContact)
     {
         throw new NotImplementedException();
     }
