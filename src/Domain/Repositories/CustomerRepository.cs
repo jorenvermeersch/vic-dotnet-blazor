ebd8e1318f9abbf2a0b4ca58d714a6978e2f9a95
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-10-27 19:18:54 +0200

    Re-added mockdata in CustomerRepository

--- src/Domain/Repositories/CustomerRepository.cs
+++ src/Domain/Repositories/CustomerRepository.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using Domain.Domain;
+using Domain.Interfaces;
 
 namespace Domain.Repositories;
 
@@ -14,9 +15,9 @@ public class CustomerRepository : EntityRepository<Customer>
 
     private void SeedData()
     {
-        // _customers.Add(new ExternalCustomer("Unizoo", "Unizo", new ContactPerson("Giles", "Van Wetteren", "giles.vanwetteren@unizo.be", "+3258691425"), new ContactPerson("Pieter", "Selie", "pieter.selie@unizo.be", "+3202691855")));
-        // _customers.Add(new ExternalCustomer("Vookaa", "Voka", new ContactPerson("Ali", "Mohammed", "ali.Mohammed@voka.be", "+3258691102"), new ContactPerson("Jen", "eever", "jen.eever@voka.be", "+3202862855")));
-        // _customers.Add(new InternalCustomer("Verpleegkunde", "vp", new ContactPerson("Robin", "Vermeire", "robin.vermeire@student.hogent.be", "+32471185226")));
-        // _customers.Add(new InternalCustomer("", "DIT", new ContactPerson("Joren", "Vermeersch", "joren.vermeersh@hogent.be", "+325869253"), new ContactPerson("Kerem", "Yilmaz", "Kerem.Yilmaz@hogent.be", "+325885253")));
+        Entities.Add(new ExternalCustomer("Unizoo", "Unizo", new ContactPerson("Giles", "Van Wetteren", "giles.vanwetteren@unizo.be", "+3258691425"), new ContactPerson("Pieter", "Selie", "pieter.selie@unizo.be", "+3202691855"), new List<IVirtualMachine>()));
+        Entities.Add(new ExternalCustomer("Vookaa", "Voka", new ContactPerson("Ali", "Mohammed", "ali.Mohammed@voka.be", "+3258691102"), new ContactPerson("Jen", "eever", "jen.eever@voka.be", "+3202862855"), new List<IVirtualMachine>()));
+        Entities.Add(new InternalCustomer("Verpleegkunde", "vp", new ContactPerson("Robin", "Vermeire", "robin.vermeire@student.hogent.be", "+32471185226"), new ContactPerson("Robin", "Vermeire", "robin.vermeire@student.hogent.be", "+32471185226"), new List<IVirtualMachine>()));
+        Entities.Add(new InternalCustomer("", "DIT", new ContactPerson("Joren", "Vermeersch", "joren.vermeersh@hogent.be", "+325869253"), new ContactPerson("Kerem", "Yilmaz", "Kerem.Yilmaz@hogent.be", "+325885253"), new List<IVirtualMachine>()));
     }
 }
