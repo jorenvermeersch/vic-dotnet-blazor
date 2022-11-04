de0b72c30a30653bced8f2cb4004fe689b36e0c4
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-11-04 16:28:04 +0100

    hide role picker menu

--- src/Client/wwwroot/scripts/index.js
+++ src/Client/wwwroot/scripts/index.js
@@ -2,3 +2,9 @@
     const menu = document.querySelector(".navigation-options");
     menu.style.display = menu.style.display == "flex" ? "none" : "flex";
 };
+
+
+const OpenRolepicker = () => {
+    const rolepicker = document.querySelector("#rolepicker");
+    rolepicker.style.display = rolepicker.style.display == "flex" ? "none" : "flex";
+}
