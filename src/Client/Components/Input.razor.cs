9587e67c06ca3b124ea20f9c9a8e6acd864412ca
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-11-05 12:04:41 +0100

    Added responsive styling for Input component

--- src/Client/Components/Input.razor.cs
+++ src/Client/Components/Input.razor.cs
@@ -1,44 +1,38 @@
 using Microsoft.AspNetCore.Components;
 
-namespace Client.Components
+namespace Client.Components;
+
+public partial class Input
 {
-    public partial class Input
+    private string _label = "Label: ";
+    private bool _required = false;
+
+    [Parameter]
+    public string PlaceHolder { get; set; } = "";
+    [Parameter]
+    public InputType Type { get; set; } = InputType.TEXT;
+    [Parameter]
+    public List<string> Items { get; set; } = new();
+    [Parameter]
+    public string Label
     {
-        private string _label = "Label: ";
-        private bool _required = false;
-        [Parameter]
-        public string PlaceHolder { get; set; } = "";
-        [Parameter]
-        public InputType Type { get; set; } = InputType.TEXT;
-        [Parameter]
-        public List<string> Items { get; set; } = new();
-        [Parameter]
-        public string Label
-        {
-            get
-            {
-                return _label;
-            }
+        get => _label;
 
-            set
-            {
-                _label = $"{value}: ";
-            }
-        }
+        set => _label = $"{value}: ";
+    }
 
-        [Parameter]
-        public string customCss { get; set; } = "";
-        [Parameter]
-        public bool Required { get; set; } = false;
-        [Parameter]
-        public int OptionalId { get; set; } = -1;
-        [Parameter]
-        public EventCallback<string> Action { get; set; } = new();
-        //[Parameter] public string Label { get; set; } = "Label: ";
-        public enum InputType
-        {
-            SELECT,
-            TEXT
-        }
+    [Parameter]
+    public string customCss { get; set; } = "";
+    [Parameter]
+    public bool Required { get; set; } = false;
+    [Parameter]
+    public int OptionalId { get; set; } = -1;
+    [Parameter]
+    public EventCallback<string> Action { get; set; } = new();
+    //[Parameter] public string Label { get; set; } = "Label: ";
+    public enum InputType
+    {
+        SELECT,
+        TEXT
     }
 }
