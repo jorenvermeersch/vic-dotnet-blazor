e6b72ed92dc8ffd39d395c71963fa0931628ae3a
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-11-15 17:52:09 +0100

    you can now add credentials to vms in vm add

---
+++ src/Client/Validation/CredentialValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Shared.Host;
+using Shared.VirtualMachine;
+using static Shared.Host.HostDto;
+
+namespace Client.Validation;
+
+public class CredentialValidation : AbstractValidator<CredentialDto>
+{
+    private readonly int _nameLength = 2;
+    public CredentialValidation()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage(string.Format(FormMessages.NOTEMPTY("Username")))
+            .MinimumLength(_nameLength).WithMessage($"Naam heeft minstens {_nameLength} characters");
+        RuleFor(x => x.PasswordHash)
+            .NotEmpty().WithMessage("Geef een waarde in");
+        RuleFor(x => x.Role)
+           .NotEmpty().WithMessage("Geef een waarde in");
+    }
+}
