b94ef91973fc3bc7eef3142dc3c09c7feab6ed49
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-11-24 21:26:16 +0100

    client change: host & virtualmachines

--- src/Client/Host/Details.razor.cs
+++ src/Client/Host/Details.razor.cs
@@ -5,46 +5,45 @@ using Shared.VirtualMachine;
 using Microsoft.AspNetCore.Components;
 
 
-namespace Client.Host
+namespace Client.Host;
+
+public partial class Details
 {
-    public partial class Details
+    [Inject] public NavigationManager? Navigation { get; set; }
+    [Inject] public IHostService? HostService { get; set; }
+
+
+    [Parameter]
+    public long Id { get; set; }
+
+    //Model
+    private HostDto.Details? host;
+
+    private Dictionary<string, Dictionary<string, string>> _server = new();
+    private IEnumerable<VirtualMachineDto.Index> virtualMachines;
+    int offset, totalVirtualMachines, totalPages = 0;
+    int selectedPage = 1;
+
+    protected override async Task OnInitializedAsync()
     {
-        [Inject] public NavigationManager? Navigation { get; set; }
-        [Inject] public IHostService? HostService { get; set; }
+        host = await HostService!.GetDetailAsync(Id);
+        _server.Add("name", new() {{"Naam", host.Name}});
+        _server.Add("resources", new() {{"vCPU", host.Specifications.Processors.ToString()}, {"Geheugen", host.Specifications.Memory.ToString()}, {"Opslag", host.Specifications.Storage.ToString()}});
+        _server.Add("remainingResources", new() {{"vCPU", host.RemainingResources.Processors.ToString()}, {"Geheugen", host.RemainingResources.Memory.ToString()}, {"Opslag", host.RemainingResources.Storage.ToString()}});
+        //virtualMachines = await virtualMachineService.GetVirtualMachinesByHostId(host.Id,offset);
+        totalVirtualMachines = virtualMachines.Count();
+        totalPages = (totalVirtualMachines / 10) + 1;
+    }
 
+    private void NavigateBack()
+    {
+        Navigation!.NavigateTo("host/list");
+    }
 
-        [Parameter]
-        public long Id { get; set; }
-
-        //Model
-        private HostDto.Details? host;
-
-        private Dictionary<string, Dictionary<string, string>> _server = new();
-        private IEnumerable<VirtualMachineDto.Index> virtualMachines;
-        int offset, totalVirtualMachines, totalPages = 0;
-        int selectedPage = 1;
-
-        protected override async Task OnInitializedAsync()
-        {
-            host = await HostService!.GetDetailAsync(Id);
-            _server.Add("name", new() {{"Naam", host.Name}});
-            _server.Add("resources", new() {{"vCPU", host.Specifications.Processors.ToString()}, {"Geheugen", host.Specifications.Memory.ToString()}, {"Opslag", host.Specifications.Storage.ToString()}});
-            _server.Add("remainingResources", new() {{"vCPU", host.RemainingResources.Processors.ToString()}, {"Geheugen", host.RemainingResources.Memory.ToString()}, {"Opslag", host.RemainingResources.Storage.ToString()}});
-            //virtualMachines = await virtualMachineService.GetVirtualMachinesByHostId(host.Id,offset);
-            totalVirtualMachines = virtualMachines.Count();
-            totalPages = (totalVirtualMachines / 10) + 1;
-        }
-
-        private void NavigateBack()
-        {
-            Navigation!.NavigateTo("host/list");
-        }
-
-        async Task ClickHandler(int pageNr)
-        {
-            offset = (pageNr - 1) * 10;
-            //virtualMachines = await virtualMachineService.GetVirtualMachinesByHostId(host.Id, offset);
-            selectedPage = pageNr;
-        }
+    async Task ClickHandler(int pageNr)
+    {
+        offset = (pageNr - 1) * 10;
+        //virtualMachines = await virtualMachineService.GetVirtualMachinesByHostId(host.Id, offset);
+        selectedPage = pageNr;
     }
 }
