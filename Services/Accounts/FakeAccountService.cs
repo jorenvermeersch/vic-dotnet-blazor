4a4f412394fa8e2af9840ebe24f37b2a09bba0ea
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2022-11-27 00:04:30 +0100

    Basic get details from one account and index

--- Services/Accounts/FakeAccountService.cs
+++ Services/Accounts/FakeAccountService.cs
@@ -4,39 +4,61 @@ using Shared.Accounts;
 
 namespace Service.Accounts;
 
-public class FakeAccountService : IAccountService
-{
+public class FakeAccountService : IAccountService {
     public readonly List<Account> accounts = new();
 
-
-    public FakeAccountService()
-    {
-        var accountsFaker = new AccountFaker();
+    public FakeAccountService() {
+        AccountFaker accountsFaker = new();
         accounts = accountsFaker.UseSeed(1337).Generate(100);
     }
 
-    public Task<AccountResponse.Create> CreateAsync(AccountRequest.Create request)
-    {
+    public Task<AccountResponse.Create> CreateAsync(AccountRequest.Create request) {
         throw new NotImplementedException();
     }
 
-    public Task DeleteAsync(AccountRequest.Delete request)
-    {
+    public Task DeleteAsync(AccountRequest.Delete request) {
         throw new NotImplementedException();
     }
 
-    public Task<AccountResponse.Edit> EditAsync(AccountRequest.Edit request)
-    {
+    public Task<AccountResponse.Edit> EditAsync(AccountRequest.Edit request) {
         throw new NotImplementedException();
     }
 
-    public Task<AccountResponse.GetDetail> GetDetailAsync(AccountRequest.GetDetail request)
-    {
-        throw new NotImplementedException();
+    public async Task<AccountResponse.GetDetail> GetDetailAsync(AccountRequest.GetDetail request) {
+        AccountResponse.GetDetail response = new();
+        var query = accounts.AsQueryable();
+
+        response.Account = query.Select(x => new AccountDto.Detail {
+            Id = x.Id,
+            Department = x.Department,
+            Education = x.Education,
+            Email = x.Email,
+            Firstname = x.Firstname,
+            Lastname = x.Lastname,
+            IsActive = x.IsActive,
+            Role = x.Role
+        }).SingleOrDefault(x => x.Id == request.AccountId) ?? new AccountDto.Detail();
+        return response;
     }
 
-    public Task<AccountResponse.GetIndex> GetIndexAsync(AccountRequest.GetIndex request)
-    {
-        throw new NotImplementedException();
+    public async Task<AccountResponse.GetIndex> GetIndexAsync(AccountRequest.GetIndex request) {
+        AccountResponse.GetIndex response = new();
+        var query = accounts.AsQueryable();
+
+        response.TotalAmount = query.Count();
+
+        query = query.Skip(request.Amount * request.Page);
+        query = query.Take(request.Amount);
+
+        query.OrderBy(x => x.Email);
+        response.Accounts = query.Select(x => new AccountDto.Index {
+            Id = x.Id,
+            Email = x.Email,
+            Firstname = x.Firstname,
+            IsActive = x.IsActive,
+            Lastname = x.Lastname,
+            Role = x.Role
+        }).ToList();
+        return response;
     }
 }
