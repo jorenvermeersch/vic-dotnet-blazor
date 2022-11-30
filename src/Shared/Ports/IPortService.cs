e488e58c94e384124c1b6fdc1914385f3c9fdb9d
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-11-30 01:31:10 +0100

    fake port service & controller

--- src/Shared/Ports/IPortService.cs
+++ src/Shared/Ports/IPortService.cs
@@ -1,7 +1,10 @@
-﻿namespace Shared.Ports;
+﻿using Shared.Hosts;
+
+namespace Shared.Ports;
 
 public interface IPortService
 {
-    Task<PortResponse.GetAll> GetAllAsync();
+    Task<PortResponse.GetAll> GetAllAsync(PortRequest.GetAll request);
+    Task<PortResponse.GetDetail> GetDetailAsync(PortRequest.GetDetail request);
 }
 
