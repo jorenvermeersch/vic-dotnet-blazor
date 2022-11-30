36af7aedded1e8386a1c4e78e629f155aaf0f574
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-11-30 01:34:34 +0100

    hostfaker fix

--- Fakers/Hosts/HostFaker.cs
+++ Fakers/Hosts/HostFaker.cs
@@ -9,11 +9,14 @@ namespace Fakers.Hosts;
 
 public class HostFaker : EntityFaker<Server>
 {
+    private readonly List<HostSpecifications> hostSpecifications = new List<HostSpecifications>();
     public HostFaker()
     {
+        hostSpecifications = new HostSpecificationsFaker().UseSeed(1337).Generate(50);
+
         CustomInstantiator(f => new Server(
             name: f.Internet.UserName(),
-            resources: new HostSpecificationsFaker(), //todo
+            resources: f.PickRandom(hostSpecifications),
             virtualMachines: (ISet<VirtualMachine>) new VirtualMachineFaker().Generate(3).ToList()
         ));
     }
