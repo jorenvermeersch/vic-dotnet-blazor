b60a41fb5af5f1a0a54c5536dd0502a30f0c0b62
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2022-12-08 18:43:32 +0100

    Host detail small fixes

--- src/Client/Hosts/Details.razor.cs
+++ src/Client/Hosts/Details.razor.cs
@@ -1,3 +1,5 @@
+using Domain.Hosts;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 using Shared.Hosts;
@@ -33,11 +35,11 @@ public partial class Details
 
         host = response.Host;
         _server.Add("name", new() { { "Naam", host.Name } });
-        _server.Add("resources", new() {{ "Geheugen", host.Specifications.Memory.ToString() }, { "Opslag", host.Specifications.Storage.ToString() } });
-        _server.Add("remainingResources", new() { { "vCPU", host.RemainingResources.VirtualProcessors.ToString() }, { "Geheugen", host.RemainingResources.Memory.ToString() }, { "Opslag", host.RemainingResources.Storage.ToString() } });
+        _server.Add("resources", new() { { "vCPUs", CalculateVirtualCpu().ToString() }, { "Geheugen", host.Specifications.Memory.ToString() }, { "Opslag", host.Specifications.Storage.ToString() } });
+        _server.Add("remainingResources", new() { { "vCPUs", host.RemainingResources.VirtualProcessors.ToString() }, { "Geheugen", host.RemainingResources.Memory.ToString() }, { "Opslag", host.RemainingResources.Storage.ToString() } });
         foreach (var p in host.Specifications.Processors)
         {
-            _processors.Add(new Dictionary<string, string>() { {"Processortype", p.Key.Name }, { "Aantal Threads", p.Key.Threads.ToString() }, { "Aantal Cores", p.Key.Cores.ToString() }, { "Virtualisatiefactor", p.Value.ToString()} });
+            _processors.Add(new Dictionary<string, string>() { {"Type", p.Key.Name }, { "Cores", p.Key.Cores.ToString() }, { "Threads", p.Key.Threads.ToString() }, { "Virtualisatiefactor", p.Value.ToString()} });
         }
         totalVirtualMachines = host?.Machines?.Count() ?? 0;
         totalPages = (totalVirtualMachines / 10) + 1;
@@ -52,5 +54,8 @@ public partial class Details
     {
         offset = (pageNr - 1) * 10;
         selectedPage = pageNr;
+    }
+    private int CalculateVirtualCpu() {
+        return host.Specifications.Processors.Select(pair => pair.Key.Cores * pair.Value).Sum();
     }
 }
