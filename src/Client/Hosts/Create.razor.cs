929b12a3535d910cb6ca5d9a0d762af17a9c5dd3
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2022-12-08 16:08:23 +0100

    Form defensive push

--- src/Client/Hosts/Create.razor.cs
+++ src/Client/Hosts/Create.razor.cs
@@ -9,35 +9,46 @@ using Shared.Hosts;
 
 namespace Client.Hosts;
 
-public partial class Create
-{
+public partial class Create {
     [Inject] public NavigationManager Navigation { get; set; } = default!;
     [Inject] public IHostService HostService { get; set; } = default!;
     [Inject] public IProcessorService ProcessorService { get; set; } = default!;
 
     private EditForm? Editform { get; set; } = new();
     private HostDto.Mutate Host { get; set; } = new();
+    public int VirtualisationFactor { get; set; } = default!;
 
     private List<ProcessorDto>? Processors { get; set; }
-    int m { get; set; } = new();
+    public ProcessorDto SelectedProcessors { get; set; }
+    public List<ProcessorDto>? ChosenProcessors { get; set; }
+    public ProcessorVirtualisation? NewProcessor { get; set; }
+    private string _customcss = "background-color: white";
 
     protected override async Task OnInitializedAsync() {
         ProcessorResponse.GetIndex processorResponse = await ProcessorService.GetIndexAsync(new ProcessorRequest.GetIndex {
-          Amount = 5
+            Amount = 5
         });
         Processors = processorResponse.Processors;
-        m = processorResponse.TotalAmount;
     }
 
-    private async void HandleValidSubmit()
-    {
-        HostResponse.Create response = await HostService.CreateAsync(new HostRequest.Create
-        {
+    private async void HandleValidSubmit() {
+        HostResponse.Create response = await HostService.CreateAsync(new HostRequest.Create {
             Host = Host
         });
         Navigation!.NavigateTo("host/" + response.HostId);
     }
     private Dictionary<string, string> MakeProcessorItems() => Processors.ToDictionary(x => x.Name.ToString(), x => JsonConvert.SerializeObject(x));
-    private void SetProcessorValue(string value) => Host.Name = "test";
+    private void SetProcessorValue(string value){
+        NewProcessor.Processor = JsonConvert.DeserializeObject<ProcessorDto>(value)!;
+    }
 
+    private void AddProcessor() {
+        Host.Specifications.Processors.Add(new KeyValuePair<ProcessorDto, int>(NewProcessor.Processor, NewProcessor.VirtualisationFactor));
+    }
+
+    public class ProcessorVirtualisation {
+        public ProcessorDto Processor { get; set; }
+        public int VirtualisationFactor { get; set; }
+
+    }
 }
