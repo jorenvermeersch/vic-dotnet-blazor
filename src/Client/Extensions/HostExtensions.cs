0890a56ddf0fbdb0368ebe73cafc22a554a26210
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-12-15 15:27:57 +0100

    Fix: Forgot extension functions

---
+++ src/Client/Extensions/HostExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Hosts;
+
+namespace Client.Extensions;
+
+public static class HostExtensions
+{
+    private static string route = "host";
+
+    public static string GetDetailUrl(this HostDto.Index host)
+    {
+        return $"{route}/{host.Id}";
+    }
+}
