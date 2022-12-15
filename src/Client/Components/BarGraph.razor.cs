9f139f50371b2132be8e5577edb81dc10d89f281
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-12-15 09:14:24 +0100

    BarGraph clean-up

---
+++ src/Client/Components/BarGraph.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Components;
+
+public partial class BarGraph
+{
+    [Parameter, EditorRequired]
+    public IList<int> Data { get; set; } = new List<int>();
+
+    [Parameter, EditorRequired]
+    public int UpperBound { get; set; }
+
+    [Parameter, EditorRequired]
+    public string Title { get; set; } = default!;
+
+    [Parameter, EditorRequired]
+    public string Unit { get; set; } = default!;
+
+    [Parameter, EditorRequired]
+    public string StartLabel { get; set; } = default!;
+
+    [Parameter, EditorRequired]
+    public string EndLabel { get; set; } = default!;
+}
